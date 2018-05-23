adaf24f2599fe6bca6ae9eeef4e790b16a802d84
Nameless <truemedian@gmail.com>
2018-05-22 19:05:21 -0500

    rewrite + google auth
    also moved firebase to init to remove the extra stack layer

--- README.md
+++ README.md
@@ -4,43 +4,44 @@ Allows you to make requests to Google's Firebase Database service
 # Installation
 ``lit install Tigerism/luvit-firebase``
 
-# Usage
+# Synchronous Execution
+This wrapper does NOT support synchronous methods, if called outside of a coroutine, all methods will return an error
+
+## Usage
 First, you need to pass the root and auth key as constructors when requiring the module.
 
 To do this, follow this example:
 
 ```lua
-local firebase = require("luvit-firebase")
-local db = firebase("ROOT","AUTH")
+local firebase = require('luvit-firebase')
+
+coroutine.wrap(function()
+	local db = firebase('ROOT', 'WEB API KEY', { email = 'example@example.com', password = 'password' })
+end)()
 ```
 
-Now, you'll be able to use some functions of the script. To get the results, you'll need to pass a callback.
+The root is ONLY the database root. (ex. ROOT for `https://mydb.firebaseio.com/` is `mydb`)  
+Your WEB API KEY can be found on your project settings.  
+Email and Password must be set up in Authentication by **ADD USER**, be aware, once you create the account, you will not be able to view the password, so write it down.
 
-Examples:
+Callbacks are not required but are supported as the last parameter to methods
+
+### Methods:
+`set` will overwrite the data in the node with either a lua table or a JSON string.  
+`update` will update values in the specific node with the payload provided. **THIS IS RECOMMENDED INSTEAD OF USING SET SO YOUR DATA DOESN'T GET WIPED**  
+`delete` will remove an entire node from the database.  
+`get` will return the table for that node.
+
+# Examples:
 ```lua
-db:set("test",{test1 = "answer1", test2 = "answer2"},function(err,res)
-	p(err,res)
-end)
+local firebase = require('luvit-firebase')
 
-db:get("test",function(err,res)
-	p(err,res)
-end)
+coroutine.wrap(function()
+	local db = firebase('ROOT', 'WEB API KEY', { email = 'example@example.com', password = 'password' })
 
-db:update("test",{test1 = "answer100"},function(err,res)
-	p(err,res)
-end)
-
-db:delete("test",function(err,res)
-	p(err,res)
-end)
-
-
+	p(db:set('test', { test1 = 'answer1', test2 = 'answer2' })
+	p(db:get('test')
+	p(db:update('test', { test1 = 'answer100', })
+	p(db:delete('test')
+end)()
 ```
-
-``set`` will overwrite the data in the node with either a key-value table or a JSON string.
-
-``update`` will update values. **THIS IS RECOMMENDED INSTEAD OF USING SET SO YOUR DATA DOESN'T GET WIPED**
-
-``delete`` will remove an entire node
-
-``get`` will return the JSON for that node.
--- init.lua
+++ init.lua
@@ -1,1 +1,122 @@
-return require("./firebase.lua")
+
+--// Thanks to Tigerism for the base api (https://github.com/Tigerism/luvit-firebase)
+
+local firebase = { }
+firebase.__index = firebase
+
+local google = require('./google')
+local http = require('coro-http')
+local json = require('json')
+
+local refresh_coro = { }
+
+local sleep
+local function reauthenticate(db)
+    refresh_coro[db.root] = refresh_coro[db.root] or coroutine.wrap(function()
+        local success, body = google.refresh(db.refresh, db.key)
+
+        if not success then
+            return print(string.format('%s | \27[1;33m[WARNING]\27[0m | %s', os.date('%F %T'), 'Request for Token Reauthentication Failed'))
+        end
+
+        db.refresh = body.refresh_token
+        db.auth = body.id_token
+        expires = os.time() + body.expires_in
+    end)
+
+    refresh_coro[db.root]()
+end
+
+local function isCoro()
+    local _, bool = coroutine.running()
+    return not bool
+end
+
+setmetatable(firebase, {
+    __call = function(this, dbroot, projectkey, authtab)
+        assert(isCoro(), 'Running Firebase Synchronously is not Supported at the moment')
+
+        assert(type(dbroot) == 'string', 'bad argument #1 to \'firebase\' (expected string, got ' .. type(dbroot) .. ')')
+        assert(type(projectkey) == 'string', 'bad argument #2 to \'firebase\' (expected string, got ' .. type(projectkey) .. ')')
+
+        dbroot = dbroot:gsub('https?://', ''):gsub('%.firebaseio%.com/?', '')
+
+        local db = { }
+        db.root = dbroot
+        db.key = projectkey
+
+        if type(authtab) == 'table' and (authtab[1] or authtab.email) and (authtab[2] or authtab.password) then
+            local success, body = google.email(authtab[1] or authtab.email, authtab[2] or authtab.password, db.key)
+
+            if not success then
+                return print(string.format('%s | \27[1;33m[WARNING]\27[0m | %s', os.date('%F %T'), 'Request for Token Authentication Failed'))
+            end
+
+            db.email = body.email
+            db.auth = body.idToken
+            db.refresh = body.refreshToken
+            db.expires = os.time() + body.expiresIn
+
+            print(string.format('%s | \27[1;32m[INFO]   \27[0m | %s', os.date('%F %T'), 'Firebase Authenticated: ' .. body.email))
+            this._authenticated = true
+        end
+        
+        return setmetatable(db, firebase)
+    end
+})
+
+local format = string.format
+local function formatNonAuth(db, node)
+    return format('https://%s.firebaseio.com/%s.json', db.root, node)
+end
+
+local function formatAuth(db, node)
+    return format('https://%s.firebaseio.com/%s.json?auth=%s', db.root, node, db.auth)
+end
+
+--// https://github.com/Tigerism/luvit-firebase/blob/master/firebase.lua#L15-L50
+function firebase:request(node, method, callback, content)
+    assert(isCoro(), 'Running Firebase Synchronously is not Supported at the moment')
+
+    if not self.key and not self.auth then return end
+
+    if self.auth and self.expires <= os.time() - 30 then
+        reauthenticate(self)
+    end
+
+    local uri = self.auth and formatAuth(self, node) or formatNonAuth(self, node)
+    
+    local headers, body = http.request(method, uri, { { 'Content-Type', 'application/json' } }, content)
+    body = json.parse(body)
+
+    if type(callback) == 'function' then
+        callback(headers.code ~= 200 and body, body)
+    end
+
+    return body
+end
+
+function firebase:get(node, callback)
+	return self:request(node, 'GET', callback)
+end
+
+function firebase:set(node, content, callback)
+    content = json.stringify(content)
+	return self:request(node, 'PUT', callback, content)
+end
+
+function firebase:update(node, content, callback)
+	content = type(content) == 'table' and json.stringify(content) or content
+	return self:request(node, 'PATCH', callback, content)
+end
+
+function firebase:push(node, content ,callback)
+	content = type(content) == 'table' and json.stringify(content) or content
+	return self:request(node, 'POST', callback, content)
+end
+
+function firebase:delete(node, callback)
+	return self:request(node, 'DELETE', callback)
+end
+
+return firebase
